c23f09912f04537a74afff50b7fd68d23a06707d
karthikdokiparthik <dokiparthikarthik@gmail.com>
2024-09-16 14:18:53 +0530

    fix: Refactor Restaurant model for better data handling and validation

---
+++ src/com/foodile/model/Restaurants.java
@@ -0,0 +1,53 @@
+package com.foodile.model;
+
+import java.util.List;
+
+public class Restaurants {
+    private String id;
+    private String name;
+    private String address;
+    private List<String> menu; // List of dish IDs
+    private List<Dish> dishes; // List of Dish objects
+
+    // Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<String> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<String> menu) {
+        this.menu = menu;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+}
